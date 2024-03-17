2755faa682f0732dcc5c25276d43aceb100bb950
Adityasusrname <adityasmailbooxx@gmail.com>
2024-03-17 21:05:16 +0530

    Server is saying hello world!

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+
+app.listen(3232,()=>{
+
+    console.log('Server started at http://localhost:3232')
+
+})
